46209343d48da33e4e3af9d40577d276bc4aa6ef
Tania Moore <tania@Tanias-MacBook-Pro.local>
2020-05-20 13:00:14 -0500

    adding statepopulation

---
+++ client/src/layouts/StatePopulation.js
@@ -0,0 +1,60 @@
+var inputState = 'AL';
+
+var states = {
+    'AL': '01',
+    'AK': '02',
+    'AZ': '03',
+    'AR': '04',
+    'CA': '05',
+    'CO': '06',
+    'CT': '07',
+    'DE': '08',
+    'DC': '09',
+    'FL': '10',
+    'GA': '11',
+    'HI': '12',
+    'ID': '13',
+    'IL': '14',
+    'IN': '15',
+    'IA': '16',
+    'KS': '17',
+    'KY': '18',
+    'LA': '19',
+    'ME': '20',
+    'MD': '21',
+    'MA': '22',
+    'MI': '23',
+    'MN': '24',
+    'MS': '25',
+    'MO': '26',
+    'MT': '27',
+    'NE': '28'
+    'NV': '29',
+    'NH': '30',
+    'NJ': '31',
+    'NM': '32',
+    'NY': '33',
+    'NC': '34',
+    'ND': '35',
+    'OH': '36',
+    'OK': '37',
+    'OR': '38',
+    'PA': '39',
+    'RI': '40',
+    'SC': '41',
+    'SD': '42',
+    'TN': '43',
+    'TX': '44',
+    'UT': '45',
+    'VT': '46',
+    'VA': '47',
+    'WA': '48',
+    'WV': '49',
+    'WI': '50',
+    'WY': '51'
+}
+
+var stateIDcode = states[inputState];
+
+console.log('Clicked on state ' + inputState);
+console.log('tanias stateID for ' + inputState + ' = ' + stateIDcode);
