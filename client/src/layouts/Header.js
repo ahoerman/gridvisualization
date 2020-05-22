9131a6f9a0b9dd8b7107840d53d78d59cd521f9c
Walter Hoerman <whoerman@gmail.com>
2020-05-22 12:22:22 -0400

    lots of changes

--- client/src/layouts/Header.js
+++ client/src/layouts/Header.js
@@ -1,18 +1,33 @@
 import React from "react";
 import '../App.css';
+import AppleseedInitiativeRingTextLogo2 from '..//AppleseedInitiativeRingTextLogo2.png';
+import { Navbar, Container, Row, Col} from 'react-bootstrap';
+
+// get fontawesome imports
+import { faCheckSquare, faBolt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Header() {
   return (
+    <Container>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          <img src={AppleseedInitiativeRingTextLogo2} alt="Appleseed Logo"  className="headerLogo d-inline-block align-top"/>
+        </Navbar.Brand> 
+      </Navbar>
     <div className = "HeaderDiv">
-      <div className="row">
-        <div className="col-2">
-          <h4>(appleseedlogo)</h4>
-        </div>
-        <div className="col-10">
-        <h1 className="maintitle">Appleseed Initiative Electric Grid Visualization</h1>
-        </div>
-      </div>
+        <Row>
+          <Col>
+            <Row>
+              <h1 className="mainTitle">Electric Grid Visualization <FontAwesomeIcon icon={faBolt} /></h1>
+            </Row> 
+            <Row>
+              <h3><FontAwesomeIcon icon={faCheckSquare} /> Click on one or more States to see and compare their energy profile</h3>
+            </Row>
+          </Col>
+        </Row>
     </div>
+    </Container>
   );
 }
 
