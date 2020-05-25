2e44a2be0aee89e3a1766cd6b3d454234b591b73
Tania Moore <tania@Tanias-MacBook-Pro.local>
2020-05-25 12:03:00 -0500

    api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,12 @@
 const router = require("express").Router();
 const db = require("../models");
+const WindModel = require("../models/wind");
+const SolarModel = require("../models/solar");
+const NuclearModel = require("../models/nuclear");
+const NaturalGasModel = require("../models/naturalgas");
+const HydroModel = require("../models/hydro");
+const CoalModel = require("../models/coal");
+const PetroleumModel = require("../models/petroleum")
 
 router.get("/state", (req, res) => {
   db.Generation.findAll({
