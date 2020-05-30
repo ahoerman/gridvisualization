ca8486a089f764295191f41c21b972d11cc7b628
Walter Hoerman <whoerman@gmail.com>
2020-05-30 12:04:25 -0400

    updating App js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import StateDoughnutChart from "./components/charts/DoughnutChart";
-import StateBarChart from "./components/charts/BarChart";
-import './App.css';
-import { Container, Row, Col} from 'react-bootstrap';
-import TopTenButtons from './components/TopTenButtons/TopTenButtons';
-import MobileInputStateDropdown from './components/MobileInput/MobileInputStateDropdown';
-import ChartContextProvider from "./components/contextProviders/ChartContextProvider";
-import USAMap from './components/USAMap';
-import useWindowSize from "./hooks/useWindowSize";
-import { faCheckSquare } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-=======
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavTabs from "./pages/NavTabs";
@@ -21,51 +5,17 @@ import Home from "./pages/Home";
 import FAQ from "./pages/FAQ";
 import PowerPlants from "./pages/PowerPlant";
 import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> 53445a87ae72ed3e3eccab17629b294b18aa2d17
 
-  function App() {
-  const { width} = useWindowSize();
-  console.log("render app")
+function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Container fluid>
-        <ChartContextProvider>
-        <>
-
-        { width > 900 ?
-        <>
-        <Row>
-        <h3><FontAwesomeIcon icon={faCheckSquare} /> Click on one or more States to see and compare their energy profile</h3>
-        </Row>
-        <Row>
-          <Col md={10}>
-            <USAMap/>
-          </Col>
-          <Col md={2}>
-            <TopTenButtons />
-          </Col>
-        </Row>
-        </>
-        :  <>
-        <Row>
-          < MobileInputStateDropdown />
-        </Row>
-        </>
-        } 
-
-        <Row>
-          <StateDoughnutChart/>
-        </Row>
-        <Row>
-          <StateBarChart />
-        </Row>
-        </>
-        
-        </ChartContextProvider>
-      </Container>
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <NavTabs />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/faq" component={FAQ} />
+        <Route exact path="/powerplants" component={PowerPlants} />
+      </div>
+    </Router>
   );
 }
 
