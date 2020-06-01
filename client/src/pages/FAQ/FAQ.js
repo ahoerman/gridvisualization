be9951aaa3daa6cb16e67c722e3676524f1da7dc
Tania Moore <tania@Tanias-MacBook-Pro.local>
2020-06-01 15:58:16 -0500

    updating FAQ

--- client/src/pages/FAQ/FAQ.js
+++ client/src/pages/FAQ/FAQ.js
@@ -4,13 +4,40 @@ import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import TabContainer from 'react-bootstrap/TabContainer';
 import { Container, Row, Col } from 'react-bootstrap';
+import coalimg from './coal.jpg';
 
+function renderRenewable() {
+  return (
+    <div>
+    <p>
+      Renewable energy, often referred to as “clean energy”, comes from natural sources or processes that are constantly replenished. For example, sunlight or wind keep shining and blowing, even if their availability depends on time and weather.
 
+    </p>
+    </div>
+  )
+}
+function renderNonrenewable() {
+  return (
+    <div>
+    <p>
+      Most of the energy consumed in the United States (89% in 2019) is from nonrenewable energy sources.
+    </p>
+
+    <img src={coalimg}/>
+    </div>
+  )
+}
 function FAQ() {
   const [key, setKey] = useState('home');
   return (
     <div>
       <NavBarTop />
+      
+      <h1>FAQ</h1>
+
+      <p>When people use electricity in their homes, the electrical power may come from a mixture of energy sources. Energy sources can be categorized as renewable or Nonrenewable Renewable (an energy source that can be easily replenished).
+      The major types of renewable energy sources are:
+</p>
       <>
         <Tabs
           id="controlled-tab-example"
@@ -24,19 +51,15 @@ function FAQ() {
         </Tabs>
 
         <Row>
-          {key === "renewable"}
+          {key ==="renewable" && renderRenewable() }
           {/* </Row>
           
         <Row> */}
-          {key === "nonrenewable"}
+          {key === "nonrenewable" && renderNonrenewable()}
         </Row>
       </>
-      <h1>FAQ</h1>
-
-          <p></p>
+     
         </div>
-    </div>
-  )
   )
 };
 
