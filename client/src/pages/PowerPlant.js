f8881a0bfb02f2c43f30f181a8bf45070dbb1b63
A Hoerman <awhoerman@me.com>
2020-06-03 20:56:01 -0400

    added fake powerplant page, css changes to faq

--- client/src/pages/PowerPlant.js
+++ client/src/pages/PowerPlant.js
@@ -1,27 +1,123 @@
 import React from "react";
 import NavBarTop from "../components/Header/NavBarTop";
+import Footer from "../components/Footer/Footer";
+import Table from "react-bootstrap/Table";
+import Container from "react-bootstrap/Container";
+import "./PowerPlant.css";
 import ScrollUpButton from "react-scroll-up-button";
+import Dropdown from "react-bootstrap/Dropdown";
 
 function PowerPlants() {
   return (
     <div>
       <NavBarTop />
+      <Container>
       <ScrollUpButton />
+      <div className="PowerplantTitle">
       <h1>Power Plants Page</h1>
       <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula. Curabitur pellentesque
-        turpis sit amet eros iaculis, a mollis arcu dictum. Ut vel ante eget massa ornare placerat.
-        Etiam nisl orci, finibus sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum
-        nunc, sed pretium risus rutrum eget. Nam consequat, ligula in faucibus vestibulum, nisi
-        justo laoreet risus, luctus luctus mi lacus sit amet libero. Class aptent taciti sociosqu ad
-        litora torquent per conubia nostra, per inceptos himenaeos. Mauris pretium condimentum
-        tellus eget lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec
-        placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum in velit pellentesque
-        vehicula sit amet at augue. Maecenas aliquam bibendum congue. Pellentesque semper, lectus
-        non ullamcorper iaculis, est ligula suscipit velit, sed bibendum turpis dui in sapien.
+        Select a state from the dropdown below to see a list of power plants in the area.
       </p>
+
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Select A State
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">New Hampshire</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Maine</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Vermont</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+      </div>
+
+      <Table striped bordered hover size="sm" className="PowerplantTable">
+        <thead>
+          <tr>
+            <th>Plant Name</th>
+            <th>Primary Energy Source</th>
+            <th>Fuel Category</th>
+            <th>Annual Generation</th>
+            <th>Annual CO2 Emission</th>
+            <th>Annual CO2 Emission/mWh</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+          </tr>
+          <tr>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+          </tr>
+          <tr>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+          </tr>
+          <tr>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+          </tr>
+          <tr>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+          </tr>
+          <tr>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+          </tr>
+          <tr>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+            <td>filler data</td>
+          </tr>
+        </tbody>
+      </Table>
+      </Container>
+      <Footer />
     </div>
   );
 }
 
 export default PowerPlants;
+
+
+
+
