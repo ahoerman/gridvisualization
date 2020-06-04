fb4a621bd93969346206fdbf90e9f4751e77c2a2
BytteL <lettybedard@gmail.com>
2020-06-03 21:18:54 -0400

    seed plant data works, rough in for generators

--- scripts/seed_plant.js
+++ scripts/seed_plant.js
@@ -19,66 +19,8 @@ let errors = [];
 let data = []; //will be parsed data from file 
 let results = []; //will be array of objects from generation data
 
-let co2Data = []; //will be array of ojbects from co2 data
-let stateData = []; //will be array of objects from state data
-let stateInfo = {}; //will be obj with abbrev:id and fullName:id pairs
-let nrgSrcObj = {}; //for storing source:id
-let nrgId; //where we will store energy id, new or not
-
 let plantData = []; //will be array of ojbects from plant data
-let plantInfo = {}; //will be obj with orisNum:plantId
 let generatorData = []; //will be array of objects from generator data
-
-
-//read and parse generation data
-fs.readFile(__dirname + fileLocation + year + "_generation_state.tsv", "utf8", (err, input) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  data = parse(input, {
-    columns: true,
-    skip_empty_lines: true,
-    delimiter: "\t"
-  });
-
-  results = data.filter((row) => {
-    return ((row["TYPE OF PRODUCER"] === "Total Electric Power Industry") && !(row["ENERGY SOURCE"] === "Total")); //we only want the total industry AND not total of all sources
-  });
-});
-
-//read and parse state pop data
-fs.readFile(__dirname + fileLocation + year + "_state_pop.tsv", "utf8", (err, input) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  stateData = parse(input, {
-    columns: true,
-    skip_empty_lines: true,
-    delimiter: "\t"
-  });
-});
-
-//read and parse co2 data
-fs.readFile(__dirname + fileLocation + year + "_emission_state.tsv", "utf8", (err, input) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  data = parse(input, {
-    columns: true,
-    skip_empty_lines: true,
-    delimiter: "\t"
-  });
-
-  co2Data = data.filter((row) => {
-    return ((row["Producer Type"] === "Total Electric Power Industry") && !(row["Energy Source"] === "All Sources")); //we only want the total industry AND not total of all sources
-  });
-});
 
 //read and parse plant data
 fs.readFile(__dirname + fileLocation + year + "_plant_test.tsv", "utf8", (err, input) => {
@@ -95,7 +37,7 @@ fs.readFile(__dirname + fileLocation + year + "_plant_test.tsv", "utf8", (err, i
 });
 
 //read and parse generator data
-fs.readFile(__dirname + fileLocation + year + "_generator_test.tsv", "utf8", (err, input) => {
+fs.readFile(__dirname + fileLocation + year + "_plant_test.tsv", "utf8", (err, input) => {
   if (err) {
     console.error(err);
     return;
@@ -151,50 +93,13 @@ const seedMe = async () => {
 
   //fill co2 emissions table
   for (const row of co2Data) {
-    // console.table(row);
+    console.table(row);
     await db.Co2Emissions.create({
       StateId: stateInfo[row.State],
       EnergySourceId: nrgSrcObj[row["Energy Source"]],
       amount: parseInt(row["CO2 (Metric Tons)"])
     });
   }
-
-  //fill plant table, save id for later
-  for (const { PSTATABB, PNAME, ORISPL, LAT, LON, PLPRMFL, PLFUELCT, PLNGENAN, PLCO2EQA, PLC2ERTA} of plantData) {
-
-    let annGen = parseInt(PLNGENAN);
-    let co2Eq = parseInt(PLCO2EQA);
-    let co2per = parseInt(PLC2ERTA);
-
-    //can't fill db with blanks, switch to 0
-    if (isNaN(annGen)) { 
-      annGen = 0; 
-    }
-    if (isNaN(co2Eq)) { 
-      co2Eq = 0; 
-    }
-    if (isNaN(co2per)) { 
-      co2per = 0; 
-    }
-
-    await db.Plant.create({
-      plantName: PNAME,
-      StateId: stateInfo[PSTATABB],
-      orisNum: ORISPL,
-      latitude: LAT,
-      longitude: LON,
-      primaryFuel: PLPRMFL,
-      fuelCategory: PLFUELCT,
-      annualGeneration: annGen,
-      annualCO2: co2Eq,
-      annualCO2perMWH: co2per
-    }).then(({ orisNum, id }) => {
-      plantInfo[orisNum] = id;
-    });
-  };
-
-
-
 };
 
 db.sequelize.sync({ 
