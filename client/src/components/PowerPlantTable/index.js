02e93cebe51f2731b39cc8bb1c4ed36b0f5f9356
BytteL <lettybedard@gmail.com>
2020-06-08 18:28:46 -0400

    singletabtable works

--- client/src/components/PowerPlantTable/index.js
+++ client/src/components/PowerPlantTable/index.js
@@ -1,39 +1,14 @@
 //Written by Letty Bedard to display the power plant data 
 
 import React from "react";
-import Table from "react-bootstrap/Table";
+import SingleTabTable from "../SingleTabTable";
 
-function PowerPlantTable({ stateName, plants }) {
+function PowerPlantTable(stateData) {
 
   return (
     <>
-      <h2>{stateName}</h2>
-      <Table striped bordered hover size="sm" className="PowerplantTable">
-        <thead>
-          <tr>
-            <th>Plant Name</th>
-            <th>Fuel Category</th>
-            <th>Primary Fuel Source</th>
-            <th>Annual Net Generation (mWh)</th>
-            <th>Annual CO2 Emission (tons)</th>
-            <th>Annual CO2 Emission/mWh (lbs/mWh)</th>
-            <th>Click To See Plant Location</th>
-          </tr>
-        </thead>
-        <tbody>
-          {plants.map((eachPlant, i) => (
-            <tr key={eachPlant.plantName+i}>
-              <td>{eachPlant.plantName}</td>
-              <td>{eachPlant.fuelCategory}</td>
-              <td>{eachPlant.primaryFuel}</td>
-              <td>{eachPlant.annualGeneration}</td>
-              <td>{eachPlant.annualCO2}</td>
-              <td>{eachPlant.annualCO2perMWH}</td>
-              <td><a href={`https://maps.google.com/?q=${eachPlant.latitude},${eachPlant.longitude}`} target="_blank" rel="noopener noreferrer" alt="powerplant location">Location</a></td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+      <h2>{stateData.stateName}</h2>
+      <SingleTabTable {...stateData} />
     </>
   );
 }
